5c8f9f4782629f8837272ae3e2e1d71c285de0ac
Shaun Biscuit <shaunbisset@hotmail.co.uk>
2025-04-15 18:00:15 +0100

    I removed the javascript from the indexhtml and put it into the javascript folder in assets for the slideshow and called it in.
    Improved my styles sheet comments to help explain during my demo.
    
    I also added styling to the intro, timeline and play button sections

---
+++ assets/javascript/slideshow.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Auto-display first slide when page loads
+document.addEventListener('DOMContentLoaded', () => showSlides(slideIndex));
